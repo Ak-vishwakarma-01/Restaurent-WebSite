73c3b55c03b5602c077622ca0ee9fc42c6c45f6e
Ankit kumar Vishwakarma <100838272+Ak-vishwakarma-01@users.noreply.github.com>
2024-08-24 23:41:39 +0530

    compliting foodDisplay

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,26 +1,33 @@
-import React, { useContext } from 'react'
-import './FoodDisplay.css'
-import { StoreContext } from '../../Context/StroeContext'
-import { FoodItem } from '../FoodItem/FoodItem'
-const FoodDisplay = (category) => {
-    const {food_list} = useContext(StoreContext) 
+import React, { useContext } from 'react';
+import './FoodDisplay.css';
+import { FoodItem } from '../FoodItem/FoodItem';
+import { StoreContext } from '../../Context/StroeContext';
+
+const FoodDisplay = ({ category }) => {  
+  const { food_list } = useContext(StoreContext);
+
   return (
     <div className='food-display' id='food-display'>
-        <h2>Top dishes near you</h2>
-        <div className="food-display-list">
-            {food_list.map((item,index)=>{
-                return <FoodItem 
+      <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          if (category === "All" || category === item.category) {
+            return (
+              <FoodItem 
                 key={index}
                 id={item.id}
                 name={item.name}
                 description={item.description}
                 price={item.price}
                 image={item.image}
-                />
-            })}
-        </div>
+              />
+            );
+          }
+          return null; // Add this to avoid returning undefined
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default FoodDisplay
+export default FoodDisplay;
--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,29 +1,46 @@
-import React, { useState } from 'react'
-import './FoodItem.css'
-import { assets } from '../../assets/assets'
+import React, { useContext } from 'react';
+import './FoodItem.css';
+import { assets } from '../../assets/assets';
+import { StoreContext } from '../../Context/StroeContext';
+
 export const FoodItem = (props) => {
-  const [itemCount,setItemCount] = useState(0)
+  const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
+
   return (
     <div className="food-item">
-        <div className="food-item-img-container">
-            <img src={props.image} alt="" className="food-item-image" />
-            {!itemCount
-              ?<img className='add' onClick={()=>setItemCount(pre=>pre+1)} src={assets.add_icon_white}/>
-              :<div className='food-item-counter'>
-                <img onClick={()=>setItemCount(pre=>pre-1)  } src={assets.remove_icon_red}/>
-                <p>{itemCount}</p>
-                <img onClick={()=>setItemCount(pre=>pre-1)} src={assets.add_icon_green}/>
-              </div>
-            }
+      <div className="food-item-img-container">
+        <img src={props.image} alt="" className="food-item-image" />
+        {!cartItems[props.id] ? (
+          <img
+            className='add'
+            onClick={() => addToCart(props.id)}
+            src={assets.add_icon_white}
+            alt="Add"
+          />
+        ) : (
+          <div className='food-item-counter'>
+            <img
+              onClick={() => removeFromCart(props.id)}
+              src={assets.remove_icon_red}
+              alt="Remove"
+            />
+            <p>{cartItems[props.id]}</p>
+            <img
+              onClick={() => addToCart(props.id)}
+              src={assets.add_icon_green}
+              alt="Add"
+            />
+          </div>
+        )}
+      </div>
+      <div className="food-item-info">
+        <div className="food-item-rating">
+          <p>{props.name}</p>
+          <img src={assets.rating_starts} alt="Rating" />
         </div>
-        <div className="food-item-info">
-            <div className="food-item-rating">
-                <p>{props.name}</p>
-                <img src={assets.rating_starts} alt=""/>
-            </div>
-            <p className="food-item-description">{props.description}</p>
-            <p className="food-item-price">${props.price}</p>
-        </div>
+        <p className="food-item-description">{props.description}</p>
+        <p className="food-item-price">${props.price}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
