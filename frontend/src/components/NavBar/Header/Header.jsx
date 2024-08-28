87c42c49378e5e069e8e42d1eaa9d6493e65039f
Ankit kumar Vishwakarma <100838272+Ak-vishwakarma-01@users.noreply.github.com>
2024-08-28 17:15:44 +0530

    fdsdgasff

--- frontend/src/components/NavBar/Header/Header.jsx
+++ frontend/src/components/NavBar/Header/Header.jsx
@@ -4,8 +4,8 @@ export const Header = () => {
   return (
     <div className='header'>
         <div className="header-content">
-            <h2>Oder lfdsjl sldjfsl</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit maiores exercitationem beatae assumenda, at necessitatibus eligendi ratione voluptatum praesentium fugiat!</p>
+            <h2>Order Your Deliciouis food here</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat expedita reprehenderit fuga itaque omnis nemo, repellat, libero doloribus possimus eveniet beatae, delectus debitis nesciunt. Iusto obcaecati ratione omnis dolorem cumque!</p>
             <button>View Menu</button>
         </div>
     </div>
